93bb82b7f058ec3c7f9a685259983272e9d036bd
RaduGiuroiu <rgiuroiu99@gmail.com>
2022-11-17 19:08:26 +0200

    Second commit

---
+++ sections/ChooseLocation.js
@@ -0,0 +1,23 @@
+const bucharest = document.querySelector(".bucharest");
+const timisoara = document.querySelector(".timisoara");
+const oradea = document.querySelector(".oradea");
+
+function updateCity(city) {
+  //updatam orasul afisat
+  const currentCity = document.getElementById("current-city");
+  currentCity.innerHTML = city;
+  //afisam noile date de la API
+  displayCurrentWeather(city);
+}
+
+bucharest.addEventListener("click", function () {
+  updateCity("București");
+});
+
+timisoara.addEventListener("click", function () {
+  updateCity("Timișoara");
+});
+
+oradea.addEventListener("click", function () {
+  updateCity("Oradea");
+});
