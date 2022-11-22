2598ff7531a6b7fd06c9210f72858734b460bc2c
RaduGiuroiu <rgiuroiu99@gmail.com>
2022-11-22 19:23:40 +0200

    advance display of forecast

--- sections/WeatherForecast.js
+++ sections/WeatherForecast.js
@@ -13,22 +13,39 @@ function displayWeatherForecast(city) {
 
       weatherForecastContainerElement.innerHTML = "";
 
+      const daysMap = {};
+
       list.forEach((element) => {
-        const { dt, main, weather } = element;
+        const { dt } = element;
 
         const day = getDayOfWeek(dt);
-        const hour = getHour(dt);
 
-        const temperature = Math.round(main.temp);
-        const realFeel = Math.round(main.feels_like);
+        if (daysMap[day]) {
+          daysMap[day].push(element);
+        } else {
+          daysMap[day] = [element];
+        }
+      });
 
-        const weatherDescription = weather[0].description;
-        const weatherIcon = getWeatherIcon(weather[0].icon);
+      for (key in daysMap) {
+        weatherForecastContainerElement.innerHTML += `<h3 class="text-primary">${key}</h3>`;
 
-        weatherForecastContainerElement.innerHTML += `
-        <div class="weather-forecast-box w-100 d-flex justify-content-between align-items-center">
+        const days = daysMap[key];
+
+        days.forEach((element) => {
+          const { dt, main, weather } = element;
+
+          const hour = getHour(dt);
+
+          const temperature = Math.round(main.temp);
+          const realFeel = Math.round(main.feels_like);
+
+          const weatherDescription = weather[0].description;
+          const weatherIcon = getWeatherIcon(weather[0].icon);
+
+          weatherForecastContainerElement.innerHTML += `
+        <div class="weather-forecast-box w-100 d-flex justify-content-between align-items-center border rounded p-3 m-3">
          <div>
-          <div><strong>${day}</strong></div>
           <div>${hour}</div>
          </div>
          <div><img src=${weatherIcon} alt="" /></div>
@@ -37,6 +54,7 @@ function displayWeatherForecast(city) {
          <div>Real feel: ${realFeel}°C</div>
         </div>
         `;
-      });
+        });
+      }
     });
 }
